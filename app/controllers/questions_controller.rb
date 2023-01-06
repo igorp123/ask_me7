7a6e624d293fb3eafde2c6389892811556869c4f
Igor Plotnikov <ig.plotnikov@gmail.com>
2023-01-06 13:34:53 +0300

    Add question partial

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   def create
     question = Question.create(questions_params)
 
-    redirect_to question_path(question)
+    redirect_to question_path(question), notice: 'Новый вопрос создан.'
   end
 
   def update
